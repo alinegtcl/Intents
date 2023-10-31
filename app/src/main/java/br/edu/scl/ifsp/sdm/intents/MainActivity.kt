f0f85e9db83f2ef5b4a7e005f433633b68638300
aline <linegcruz@gmail.com>
2023-10-31 19:17:50 -0300

    Added communication between the Main and Parameter Activities with passing parameters in an obsolete and current method.

--- app/src/main/java/br/edu/scl/ifsp/sdm/intents/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/intents/MainActivity.kt
@@ -1,26 +1,59 @@
 package br.edu.scl.ifsp.sdm.intents
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.scl.ifsp.sdm.intents.Extras.PARAMETER_EXTRA
 import br.edu.scl.ifsp.sdm.intents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+//    Não é mais necessário, devido a retirada da obsolescência
+//    companion object{
+//        private const val PARAMETER_REQUEST_CODE = 0
+//    }
     private val activityMainBinding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private lateinit var parameterArl: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
         setSupportActionBar(activityMainBinding.toolbarIn.toolbar)
         supportActionBar?.subtitle = localClassName
 
-        activityMainBinding.parameterBt.setOnClickListener {
+        parameterArl = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result ->
+            if(result.resultCode == RESULT_OK) {
+                result.data?.getStringExtra(PARAMETER_EXTRA)?.also {
+                    activityMainBinding.parameterTv.text = it
+                }
+            }
+        }
 
+        activityMainBinding.apply {
+            parameterBt.setOnClickListener {
+                val parameterIntent = Intent(this@MainActivity, ParameterActivity::class.java).apply {
+                    putExtra(PARAMETER_EXTRA, parameterTv.text)
+                }
+                parameterArl.launch(parameterIntent)
+                // startActivityForResult(parameterIntent, PARAMETER_REQUEST_CODE) obsoleto, no vídeo foi retirado
+            }
         }
     }
+
+//    Forma depreciada, no vídeo foi retirada
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == PARAMETER_REQUEST_CODE && resultCode == RESULT_OK) {
+//            data?.getStringExtra(PARAMETER_EXTRA)?.also {
+//                activityMainBinding.parameterTv.text = it
+//            }
+//        }
+//    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -32,6 +65,7 @@ class MainActivity : AppCompatActivity() {
             R.id.openActivityMi -> {
                 true
             }
+
             R.id.viewMi -> {
                 true
             }
